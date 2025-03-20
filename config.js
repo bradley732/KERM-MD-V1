10fcc9f89ac35506b00554c548697f3acff61aa0
bradley732 <bradleytafadzwamunyadza@gmail.com>
2025-03-20 15:21:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "KERM-MD-V1~GIY3FAZT#xIoqYY4fZ7vEFwyQFGwTsYbG1ylbdfh07Nbvkm7DgFw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
